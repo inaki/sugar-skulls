82d4b73399369b4d5d6a85078e8d3fdeeb6c8b15
Imanol Aranzadi <iiaranzadi@gmail.com>
2014-10-20 13:53:33 -0400

    added the new prototype called indes1

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-angular.module('skullApp', ['ui.bootstrap', 'ngClipboard'])
+angular.module('skullApp', [])
   // .config(['ngClipProvider', function(ngClipProvider) {
   //   ngClipProvider.setPath("//cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.1.6/ZeroClipboard.swf");
   //   }])
   .controller('mainCtrl', function($scope){
-      
+
 
   });
