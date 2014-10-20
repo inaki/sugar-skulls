1d39190bc902b6de678bd2115ba0821e0cb0ac30
Imanol Aranzadi <iiaranzadi@gmail.com>
2014-10-20 08:31:33 -0400

    added angular boiler plate

---
+++ js/app.js
@@ -0,0 +1,8 @@
+angular.module('skullApp', ['ui.bootstrap', 'ngClipboard'])
+  // .config(['ngClipProvider', function(ngClipProvider) {
+  //   ngClipProvider.setPath("//cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.1.6/ZeroClipboard.swf");
+  //   }])
+  .controller('mainCtrl', function($scope){
+      
+
+  });
